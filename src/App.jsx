7d815abf70932f1d3ee2c974703c0684610b9305
leejiwon1234 <asmonnbbv12@gmail.com>
2025-04-26 17:40:11 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,24 @@ import profileImg from './assets/images/zi-a.png';
 function App() {
   return (
     <div className="main-container">
-      <h1 className="title">정지아의 리액트 ✨</h1>
+      <h1 className="title">이지원의 리액트 ✨</h1>
 
       <ProfileCard
         image={profileImg}
-        name="정지아"
-        description="신소재를 사랑하는 개발자입니다."
+        name="이지원"
+        description="개발자입니다."
       />
 
       <div className="link-area">
         <a
           className="link-button"
-          href="https://chapter3-react1.vercel.app/"
+          href="https://coffee-one-lime.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📘 CHAPTER 3 - 기초
+          ☕ COFFEE
         </a>
-        <a
-          className="link-button"
-          href="https://chapter3-react2.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          📙 CHAPTER 3 게시판 CRUD
-        </a>
-        <a
-          className="link-button"
-          href="https://chapter3-react3.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          📗 CHAPTER 3 게시판 + MySQL
-        </a>
+     
       </div>
     </div>
   );
